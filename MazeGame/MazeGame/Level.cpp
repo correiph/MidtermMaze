1b50a649ef74a3355a29c4c51f19e0e6adc6aa89
correiph <philipcorreia@gmail.com>
2015-11-08 02:58:13 -0500

    base

--- MazeGame/MazeGame/Level.cpp
+++ MazeGame/MazeGame/Level.cpp
@@ -5,7 +5,7 @@
 Level::Level()
 {
 	player_texture.loadFromFile("Player_Sprite_Sheet_32_32.png");
-	m_player = new Player(sf::Vector2f(0,0), sf::Vector2f(16, 16), 100.0f, player_texture);
+	m_player = new Player(sf::Vector2f(0,0), sf::Vector2f(32, 32), 100.0f, player_texture);
 	//m_player->sprite.
 	m.loadFromFile("level1.tmx");
 	//player_texture.loadFromFile("playersprite.png");
@@ -20,33 +20,41 @@ Level::~Level()
 void Level::Update(float delta){
 	if (sf::Keyboard::isKeyPressed(sf::Keyboard::W) && sf::Keyboard::isKeyPressed(sf::Keyboard::A)) {
 		m_player->SetDirection(sf::Vector2f(-1, -1));
+		m_player->SetAnimDirection(Player::PLAYER_DIRECTION::UPLEFT);
 	}
 	else if (sf::Keyboard::isKeyPressed(sf::Keyboard::W) && sf::Keyboard::isKeyPressed(sf::Keyboard::D)) {
 		m_player->SetDirection(sf::Vector2f(1, -1));
+		m_player->SetAnimDirection(Player::PLAYER_DIRECTION::UPRIGHT);
 	}
 	else if (sf::Keyboard::isKeyPressed(sf::Keyboard::S) && sf::Keyboard::isKeyPressed(sf::Keyboard::A)) {
 		m_player->SetDirection(sf::Vector2f(-1, 1));
+		m_player->SetAnimDirection(Player::PLAYER_DIRECTION::DOWNLEFT);
 	}
 	else if (sf::Keyboard::isKeyPressed(sf::Keyboard::S) && sf::Keyboard::isKeyPressed(sf::Keyboard::D)) {
 		m_player->SetDirection(sf::Vector2f(1, 1));
+		m_player->SetAnimDirection(Player::PLAYER_DIRECTION::DOWNRIGHT);
 	}
 	else if (sf::Keyboard::isKeyPressed(sf::Keyboard::W)) {
 		m_player->SetDirection(sf::Vector2f(0, -1));
+		m_player->SetAnimDirection(Player::PLAYER_DIRECTION::UP);
 	}
 	else if (sf::Keyboard::isKeyPressed(sf::Keyboard::S)) {
 		m_player->SetDirection(sf::Vector2f(0, 1));
+		m_player->SetAnimDirection(Player::PLAYER_DIRECTION::DOWN);
 	}
 	else if (sf::Keyboard::isKeyPressed(sf::Keyboard::A)) {
 		m_player->SetDirection(sf::Vector2f(-1, 0));
+		m_player->SetAnimDirection(Player::PLAYER_DIRECTION::LEFT);
 	}
 	else if (sf::Keyboard::isKeyPressed(sf::Keyboard::D)) {
 		m_player->SetDirection(sf::Vector2f(1, 0));
+		m_player->SetAnimDirection(Player::PLAYER_DIRECTION::RIGHT);
 	}
 	else {
 		m_player->SetDirection(sf::Vector2f(0, 0));
+		m_player->SetAnimDirection(Player::PLAYER_DIRECTION::IDLE);
 	}
 	m_player->Move(delta);
-<<<<<<< HEAD
 	if (m_player->Right()>320){
 		sf::Vector2f dir = *m_player->GetDirection();
 		dir.x *= -1;
@@ -97,8 +105,6 @@ void Level::Update(float delta){
 		}
 	}
 	//m_player->GetBoundingBox().intersects()
-=======
->>>>>>> master
 }
 void Level::draw(sf::RenderTarget& target, sf::RenderStates states) const {
 	target.draw(m, states);
