52c35618beecfa806fe6c115a44083e80b1a9ace
correiph <philipcorreia@gmail.com>
2015-11-07 23:53:34 -0500

    Revert 3c3ef1178df97a1c220bd241b8c3aa6902f4468a

--- MazeGame/MazeGame/Entity.h
+++ MazeGame/MazeGame/Entity.h
@@ -10,10 +10,6 @@ public:
 	sf::FloatRect GetBoundingBox() {
 		return sf::FloatRect(m_position.x, m_position.y, m_size.x, m_size.y);
 	}
-	float Top(){ return m_position.y; };
-	float Bottom(){ return m_position.y + m_size.y; };
-	float Left(){ return m_position.x; };
-	float Right(){ return m_position.x + m_size.x; };
 protected:
 	sf::Vector2f m_position;
 	sf::Vector2f m_size;
